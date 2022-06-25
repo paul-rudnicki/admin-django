91fc176335be93dc1040ca9a4f98b9a81517d15a
Paweł Rudnicki <pawel.rudnicki@lama-media.com>
2022-06-25 17:53:12 +0200

    urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path('', home, name='dashboard')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                        document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-                        document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                    document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                    document_root=settings.STATIC_ROOT)
