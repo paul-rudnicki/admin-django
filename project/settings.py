64b9704dfc687346dc387f51b1cab1532f4f3e18
Paweł Rudnicki <pawel.rudnicki@lama-media.com>
2022-06-25 17:15:17 +0200

    hosts 2

--- project/settings.py
+++ project/settings.py
@@ -20,9 +20,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -33,7 +30,6 @@ else:
     DEBUG = os.environ.get('DEBUG_PROD')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'devsolutions.atthost24.pl']
-
 
 # Application definition
 
