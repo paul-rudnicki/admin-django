f4f4fbf472c3f3b84c6bb1be19358cf77d655b55
Paweł Rudnicki <pawel.rudnicki@lama-media.com>
2022-06-25 18:06:03 +0200

    static

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,13 +1,21 @@
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.static import serve
 from .views import home
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('', home, name='dashboard')
+static_urlpatterns = [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^assets/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-                    document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,
-                    document_root=settings.STATIC_ROOT)
+urlpatterns = [
+    path('', home, name='dashboard'),
+    path("", include(static_urlpatterns))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                        document_root=settings.STATIC_ROOT)
