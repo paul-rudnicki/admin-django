4cdbf3d03f2cde7ef1e249d93ddf1c71ff6fcc69
Paweł Rudnicki <pawel.rudnicki@lama-media.com>
2022-06-25 17:13:43 +0200

    hosts

--- project/settings.py
+++ project/settings.py
@@ -26,13 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# if (len(sys.argv) >= 2 and sys.argv[1] == 'runserver'):
-#     DEBUG = os.environ.get('DEBUG_LOCAL')
-# else:
-#     DEBUG = os.environ.get('DEBUG_PROD')
-
-DEBUG = True
+SECURITY WARNING: don't run with debug turned on in production!
+if (len(sys.argv) >= 2 and sys.argv[1] == 'runserver'):
+    DEBUG = os.environ.get('DEBUG_LOCAL')
+else:
+    DEBUG = os.environ.get('DEBUG_PROD')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'devsolutions.atthost24.pl']
 
